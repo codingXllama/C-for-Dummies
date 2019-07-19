7f1475df346b8cfe4470b5fa6a0882b75f89c2e4
Osama Al-Chalabi <osamaalchalabi2@gmail.com>
2019-07-19 16:54:41 -0600

    Created Variable example and note

---
+++ BasicIO/main.cpp
@@ -0,0 +1,11 @@
+//Section 5
+//Basic I/O using cin and cout
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,18 @@
+/*
+    Variables have 3 aspects to them
+    1. Name - what you name want to use to refer to your variable (what you want your box be called)
+    2. Type - what is the kind of data this variable  stores in it's box
+    3 Value - the actual meat/content this variable stores in it's box.
+
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int grade1, grade2, grade3;
+    grade1 = 10, grade2 = 3, grade3 = 122;
+    cout << "Hello you are " << grade2 << " years old!" << endl;
+    return 0;
+}
